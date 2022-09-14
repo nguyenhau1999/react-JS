e5bc63765dfe2a24e3e16a9e51c5d800d55466b1
nguyenhau1999 <nguyenhau9091999@gmail.com>
2022-09-14 19:57:08 +0700

    State

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 class MyComponent extends React.Component{
     
+/**
+ * key: value
+  */
+ state = {
+    name: 'Hau',
+    channel: 'React JS'
+ }
+ /*  
+ fragment
+ */
+
     render (){
+       
         return (
-            <div>hello my Component Hau</div>
+       <>
+            <div className="first">
+                hello my Component , My name is {this.state['name']}
+            </div>
+            <div className="second">
+            My channel is : {this.state.channel}
+            </div>
+           
+            
+        </>
         )
     }
 }
