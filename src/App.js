efb26765fc6444287ce99b1767f3d3691c2517e5
nguyenhau1999 <nguyenhau9091999@gmail.com>
2022-09-14 19:34:43 +0700

    MyComponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './views/Example/MyComponent.js';
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent></MyComponent>
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MyComponent extends React.Component{
+    
+    render (){
+        return (
+            <div>hello my Component Hau</div>
+        )
+    }
+}
+
+export default MyComponent;
