771fb9c167c021bccd14f8b771345744ee423f10
nguyenhau1999 <nguyenhau9091999@gmail.com>
2022-09-24 03:09:59 +0700

    Redux Setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './store/reducers/rootReducer';
+
+const reduxStore = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={reduxStore}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    users: [
+        { id: 1, name: 'Hau' },
+        { id: 2, name: 'Front end'}
+    ]
+} 
+
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+export default rootReducer;
